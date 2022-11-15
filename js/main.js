c710a29202611254c720a07b6f93ac443b6f1a2c
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-11-15 16:02:02 +0100

    Aggiunge form con push all'interno della lista

--- js/main.js
+++ js/main.js
@@ -3,8 +3,21 @@ const {createApp} = Vue;
 
 createApp({
     data(){
+        
         return{
-          
-        }
+                item: "",
+                listItem : [ ],
+
     }
+},
+    methods:{
+                    createItem(){
+                        let userList = {
+                            text: this.item,
+                            done: false,
+                        }
+                        this.listItem.push(userList);                   
+                       this.item = '';
+        },
+                    },
 }).mount("#app");
