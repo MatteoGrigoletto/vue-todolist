e2af10b41fe2a0845a2b3aa9bea6e300b12868e4
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-11-15 16:18:10 +0100

    Aggiunge bonus. Manca solamente il css

--- js/main.js
+++ js/main.js
@@ -18,9 +18,17 @@ createApp({
                         }
                         this.listItem.push(userList);                   
                        this.item = '';
-        },
+                    },
                     spliceItem(index){
                         this.listItem.splice(index, 1);
                     },
-                    },
+                    invertDone(element){
+                        if(element.done === false){
+                            element.done = true;
+                        }
+                        else{
+                            element.done = false;
+                        } 
+                    }
+                },
 }).mount("#app");
