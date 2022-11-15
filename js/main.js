ad5aba6c5456391ab64cef1c0ca3a685bad126a7
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-11-15 16:04:17 +0100

    Aggiunge rimozione elemento lista tramite un click

--- js/main.js
+++ js/main.js
@@ -19,5 +19,8 @@ createApp({
                         this.listItem.push(userList);                   
                        this.item = '';
         },
+                    spliceItem(index){
+                        this.listItem.splice(index, 1);
+                    },
                     },
 }).mount("#app");
